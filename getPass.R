a457570d650c29c4e1e44d4e3db611582b5b603e
toosuto-r <drrmccarron@gmail.com>
2016-06-13 00:32:10 +0100

    Added interface to a percentage table of words without subreddit distinction in wordTraceAnalysis

--- getPass.R
+++ getPass.R
@@ -1,5 +1,9 @@
-getPass<- function(){
-  pw<-read.csv(file="C:/Users/Ryan/Documents/R/data/pw_remote.txt", header = FALSE)
+getPass<- function(location){
+  if (location=="local"){
+    pw<-read.csv(file="C:/postgres/pw.txt", header = FALSE)
+  } else {
+    pw<-read.csv(file="C:/Users/Ryan/Documents/R/data/pw_remote.txt", header = FALSE)
+  }
   pw<-as.character(pw[1,1])
   return(pw)
 }
